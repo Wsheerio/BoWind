a737b989c5da4c493211944882b1c8a1b9ca2464
Wsheerio <trivial.derp@gmail.com>
2015-08-22 10:46:49 -0400

    setting and option is now separated with ":"

--- Program.cs
+++ Program.cs
@@ -4,7 +4,6 @@ using System.Globalization;
 using System.Diagnostics;
 using System.Threading;
 using System.Windows;
-using System.Linq;
 using System.Text;
 using System.IO;
 using System;
@@ -25,7 +24,9 @@ namespace BoWind
         [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
         static extern int GetWindowText(IntPtr hWnd, StringBuilder lpString, int nMaxCount);
         static Dictionary<string, string> config = new Dictionary<string, string>();
-        static Dictionary<double, List<Dictionary<char, string>>> programs = new Dictionary<double, List<Dictionary<char, string>>>();
+        static List<Dictionary<char, string>> programs = new List<Dictionary<char, string>>();
+        static double monitorWidth = SystemParameters.PrimaryScreenWidth;
+        static double monitorHeight = SystemParameters.PrimaryScreenHeight;
         static void Main(string[] args)
         {
             if (!File.Exists("config.txt"))
@@ -46,83 +47,78 @@ namespace BoWind
             string[] command;
             double width = 0;
             double height = 0;
-            double monitorWidth = SystemParameters.PrimaryScreenWidth;
-            double monitorHeight = SystemParameters.PrimaryScreenHeight;
             while (1 == 1)
             {
                 Thread.Sleep(1000);
                 foreach (Process process in Process.GetProcesses())
                 {
-                    foreach (KeyValuePair<double, List<Dictionary<char, string>>> item in programs)
+                    foreach (Dictionary<char, string> value in programs)
                     {
-                        foreach (Dictionary<char, string> value in item.Value)
+                        if (process.ProcessName == value['n'])
                         {
-                            if (process.ProcessName == value['n'])
+                            string[] shift = value['s'].Split(',');
+                            if (Convert.ToDouble(value['a'].Split(',')[0]) / Convert.ToDouble(value['a'].Split(',')[1]) >= monitorWidth / monitorHeight)
                             {
-                                string[] temp = value['s'].Split(',');
-                                if (item.Key >= monitorWidth / monitorHeight)
+                                width = monitorWidth;
+                                height = monitorWidth / (Convert.ToDouble(value['a'].Split(',')[0]) / Convert.ToDouble(value['a'].Split(',')[1]));
+                            }
+                            else
+                            {
+                                width = monitorHeight * Convert.ToDouble(value['a'].Split(',')[0]) / Convert.ToDouble(value['a'].Split(',')[1]);
+                                height = monitorHeight;
+                            }
+                            if (value['c'] == "true")
+                            {
+                                uint compareA;
+                                uint compareB;
+                                List<IntPtr> handles = new List<IntPtr>();
+                                handles.Add(process.MainWindowHandle);
+                                IntPtr previousHandle = process.MainWindowHandle;
+                                while (GetWindow(previousHandle, 3) != IntPtr.Zero)
                                 {
-                                    width = monitorWidth;
-                                    height = monitorWidth / item.Key;
+                                    handles.Add(GetWindow(previousHandle, 3));
+                                    previousHandle = GetWindow(previousHandle, 3);
                                 }
-                                else
+                                previousHandle = process.MainWindowHandle;
+                                while (GetWindow(previousHandle, 2) != IntPtr.Zero)
                                 {
-                                    width = monitorHeight * item.Key;
-                                    height = monitorHeight;
+                                    handles.Add(GetWindow(previousHandle, 2));
+                                    previousHandle = GetWindow(previousHandle, 2);
                                 }
-                                if (value['c'] == "true")
+                                foreach (IntPtr hand in handles)
                                 {
-                                    uint compareA;
-                                    uint compareB;
-                                    List<IntPtr> handles = new List<IntPtr>();
-                                    handles.Add(process.MainWindowHandle);
-                                    IntPtr previousHandle = process.MainWindowHandle;
-                                    while (GetWindow(previousHandle, 3) != IntPtr.Zero)
+                                    GetWindowThreadProcessId(hand, out compareA);
+                                    GetWindowThreadProcessId(process.MainWindowHandle, out compareB);
+                                    if (compareA == compareB)
                                     {
-                                        handles.Add(GetWindow(previousHandle, 3));
-                                        previousHandle = GetWindow(previousHandle, 3);
-                                    }
-                                    previousHandle = process.MainWindowHandle;
-                                    while (GetWindow(previousHandle, 2) != IntPtr.Zero)
-                                    {
-                                        handles.Add(GetWindow(previousHandle, 2));
-                                        previousHandle = GetWindow(previousHandle, 2);
-                                    }
-                                    foreach (IntPtr hand in handles)
-                                    {
-                                        GetWindowThreadProcessId(hand, out compareA);
-                                        GetWindowThreadProcessId(process.MainWindowHandle, out compareB);
-                                        if (compareA == compareB)
+                                        StringBuilder stringBuilder = new StringBuilder(1024);
+                                        GetWindowText(hand, stringBuilder, stringBuilder.Capacity);
+                                        if (stringBuilder.ToString() != "" && stringBuilder.ToString() != "Default IME" && stringBuilder.ToString() != "MSCTFIME UI")
                                         {
-                                            StringBuilder stringBuilder = new StringBuilder(1024);
-                                            GetWindowText(hand, stringBuilder, stringBuilder.Capacity);
-                                            if (stringBuilder.ToString() != "" && stringBuilder.ToString() != "Default IME" && stringBuilder.ToString() != "MSCTFIME UI")
+                                            SetWindowLong(hand, -16, 0x0008 | 0x00C);
+                                            SetWindowPos(hand, 0, Convert.ToInt16(monitorWidth / 2 - width / 2 - Convert.ToInt16(shift[0])), Convert.ToInt16(monitorHeight / 2 - height / 2 - Convert.ToInt16(shift[1])), Convert.ToInt16(width) + Convert.ToInt16(shift[2]), Convert.ToInt16(height) + Convert.ToInt16(shift[3]), 0x0400);
+                                            ShowWindow(hand, 5);
+                                            if (config["debug"] == "true")
                                             {
-                                                SetWindowLong(hand, -16, 0x0008 | 0x00C);
-                                                SetWindowPos(hand, 0, Convert.ToInt16(monitorWidth / 2 - width / 2 - Convert.ToInt16(temp[0])), Convert.ToInt16(monitorHeight / 2 - height / 2 - Convert.ToInt16(temp[1])), Convert.ToInt16(width) + Convert.ToInt16(temp[2]), Convert.ToInt16(height) + Convert.ToInt16(temp[3]), 0x0400);
-                                                ShowWindow(hand, 5);
-                                                if (config["debug"] == "true")
-                                                {
-                                                    Console.WriteLine(hand + " " + stringBuilder.ToString());
-                                                }
+                                                Console.WriteLine(hand + " " + stringBuilder.ToString());
                                             }
                                         }
                                     }
                                 }
-                                else
-                                {
-                                    SetWindowLong(process.MainWindowHandle, -16, 0x0008 | 0x00C);
-                                    SetWindowPos(process.MainWindowHandle, 0, Convert.ToInt16(monitorWidth / 2 - width / 2 - Convert.ToInt16(temp[0])), Convert.ToInt16(monitorHeight / 2 - height / 2 - Convert.ToInt16(temp[1])), Convert.ToInt16(width) + Convert.ToInt16(temp[2]), Convert.ToInt16(height) + Convert.ToInt16(temp[3]), 0x0400);
-                                    ShowWindow(process.MainWindowHandle, 5);
-                                    if (config["debug"] == "true")
-                                    {
-                                        Console.WriteLine(process.MainWindowHandle + " " + process.MainWindowTitle);
-                                    }
-                                }
+                            }
+                            else
+                            {
+                                SetWindowLong(process.MainWindowHandle, -16, 0x0008 | 0x00C);
+                                SetWindowPos(process.MainWindowHandle, 0, Convert.ToInt16(monitorWidth / 2 - width / 2 - Convert.ToInt16(shift[0])), Convert.ToInt16(monitorHeight / 2 - height / 2 - Convert.ToInt16(shift[1])), Convert.ToInt16(width) + Convert.ToInt16(shift[2]), Convert.ToInt16(height) + Convert.ToInt16(shift[3]), 0x0400);
+                                ShowWindow(process.MainWindowHandle, 5);
                                 if (config["debug"] == "true")
                                 {
-                                    Console.WriteLine(value['n'] + " compat " + value['c'] + " " + item.Key + " " + Convert.ToInt32(monitorWidth / 2 - width / 2) + " - " + temp[0] + " " + Convert.ToInt16(monitorHeight / 2 - height / 2) + " - " + temp[1] + " " + width + " + " + temp[2] + " " + height + " + " + temp[3] + "\n");
+                                    Console.WriteLine(process.MainWindowHandle + " " + process.MainWindowTitle);
                                 }
+                            }
+                            if (config["debug"] == "true")
+                            {
+                                Console.WriteLine(value['n'] + " compat " + value['c'] + " " + Convert.ToString(Convert.ToDouble(value['a'].Split(',')[0]) / Convert.ToDouble(value['a'].Split(',')[1])) + " " + Convert.ToInt32(monitorWidth / 2 - width / 2) + " - " + shift[0] + " " + Convert.ToInt16(monitorHeight / 2 - height / 2) + " - " + shift[1] + " " + width + " + " + shift[2] + " " + height + " + " + shift[3] + "\n");
                             }
                         }
                     }
@@ -155,7 +151,7 @@ namespace BoWind
             int checkInt;
             bool checkBool;
             config = new Dictionary<string, string>();
-            programs = new Dictionary<double, List<Dictionary<char, string>>>();
+            programs = new List<Dictionary<char, string>>();
             config.Add("debug", "false");
             config.Add("background", "false");
             config.Add("backgroundcolor", "000000");
@@ -186,34 +182,34 @@ namespace BoWind
             {
                 ShowWindow(Process.GetCurrentProcess().MainWindowHandle, 0);
             }
-            foreach (string line in File.ReadAllLines("programs.txt"))
+            for (int i = 0; i < File.ReadAllLines("programs.txt").Length; i++)
             {
-                string[] temp = line.Split(':');
-                programs.Add(Convert.ToDouble(temp[0]) / Convert.ToDouble(temp[1]), new List<Dictionary<char, string>>());
-                for (int i = 2; i < temp.Length; i++)
+                programs.Add(new Dictionary<char, string> { { 'a', Convert.ToString(monitorWidth) + ',' + Convert.ToString(monitorHeight) }, { 'c', "false" }, { 'n', "default" }, { 's', "0,0,0,0" } });
+                string[] values = File.ReadAllLines("programs.txt")[i].Split('.');
+                for (int j = 0; j < values.Length; j++)
                 {
-                    string[] values = temp[i].Split('.');
-                    programs[Convert.ToDouble(temp[0]) / Convert.ToDouble(temp[1])].Add(new Dictionary<char, string> { { 'c', "false" }, { 'n', "default" }, { 's', "0,0,0,0" } });
-                    for (int j = 0; j < values.Length; j++)
+                    if (values[j].Split(':').Length == 2)
                     {
-                        if (values[j].Split(' ').Length == 2)
+                        if (programs[i].ContainsKey(Convert.ToChar(values[j].Split(':')[0])))
                         {
-                            if (programs[Convert.ToDouble(temp[0]) / Convert.ToDouble(temp[1])][i - 2].ContainsKey(Convert.ToChar(values[j].Split(' ')[0])))
-                            {
-                                programs[Convert.ToDouble(temp[0]) / Convert.ToDouble(temp[1])][i - 2][Convert.ToChar(values[j].Split(' ')[0])] = values[j].Split(' ')[1];
-                            }
+                            programs[i][Convert.ToChar(values[j].Split(':')[0])] = values[j].Split(':')[1];
                         }
                     }
-                    if (programs[Convert.ToDouble(temp[0]) / Convert.ToDouble(temp[1])][i - 2]['s'].Split(',').Length < 4 || !int.TryParse(programs[Convert.ToDouble(temp[0]) / Convert.ToDouble(temp[1])][i - 2]['s'].Split(',')[0], out checkInt) || !int.TryParse(programs[Convert.ToDouble(temp[0]) / Convert.ToDouble(temp[1])][i - 2]['s'].Split(',')[1], out checkInt) || !int.TryParse(programs[Convert.ToDouble(temp[0]) / Convert.ToDouble(temp[1])][i - 2]['s'].Split(',')[2], out checkInt) || !int.TryParse(programs[Convert.ToDouble(temp[0]) / Convert.ToDouble(temp[1])][i - 2]['s'].Split(',')[3], out checkInt))
-                    {
-                        programs[Convert.ToDouble(temp[0]) / Convert.ToDouble(temp[1])][i - 2]['s'] = "0,0,0,0";
-                        MessageBox.Show("Border shift options for " + values[0] + " ignored.");
-                    }
-                    if (!bool.TryParse(programs[Convert.ToDouble(temp[0]) / Convert.ToDouble(temp[1])][i - 2]['c'], out checkBool))
-                    {
-                        programs[Convert.ToDouble(temp[0]) / Convert.ToDouble(temp[1])][i - 2]['c'] = "false";
-                        MessageBox.Show("Compatibility options for " + values[0] + " ignored.");
-                    }
+                }
+                if (programs[i]['s'].Split(',').Length < 4 || !int.TryParse(programs[i]['s'].Split(',')[0], out checkInt) || !int.TryParse(programs[i]['s'].Split(',')[1], out checkInt) || !int.TryParse(programs[i]['s'].Split(',')[2], out checkInt) || !int.TryParse(programs[i]['s'].Split(',')[3], out checkInt))
+                {
+                    programs[i]['s'] = "0,0,0,0";
+                    MessageBox.Show("Border shift options for " + values[0] + " ignored.");
+                }
+                if (programs[i]['a'].Split(',').Length < 2 || !int.TryParse(programs[i]['a'].Split(',')[0], out checkInt) || !int.TryParse(programs[i]['a'].Split(',')[1], out checkInt))
+                {
+                    programs[i]['a'] = Convert.ToString(monitorWidth) + ',' + Convert.ToString(monitorHeight);
+                    MessageBox.Show("Aspect ratio options for " + values[0] + " ignored.");
+                }
+                if (!bool.TryParse(programs[i]['c'], out checkBool))
+                {
+                    programs[i]['c'] = "false";
+                    MessageBox.Show("Compatibility options for " + values[0] + " ignored.");
                 }
             }
         }
