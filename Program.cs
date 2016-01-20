83bf7bf80dcb17303194d84f503962d974745e52
Wsheerio <trivial.derp@gmail.com>
2016-01-20 13:35:28 -0500

    A lot of changes
    I've added quite a few new features as well as changes how programs.txt
    is read.

--- Program.cs
+++ Program.cs
@@ -27,6 +27,8 @@ namespace BoWind
         static List<Dictionary<char, string>> programs = new List<Dictionary<char, string>>();
         static double monitorWidth = SystemParameters.PrimaryScreenWidth;
         static double monitorHeight = SystemParameters.PrimaryScreenHeight;
+        static double width = 0;
+        static double height = 0;
         static void Main(string[] args)
         {
             if (!File.Exists("config.txt"))
@@ -43,11 +45,42 @@ namespace BoWind
 
                 }
             }
-            loadFiles();
-            string[] command;
-            double width = 0;
-            double height = 0;
-            while (1 == 1)
+            LoadConfig();
+            LoadPrograms(File.ReadAllLines("programs.txt"));
+            args = Environment.GetCommandLineArgs();    
+            if (args.Length > 1)
+            {
+                for (int i = 0; i < programs.Count; i++)
+                {
+                    if (programs[i]['n'] == args[1])
+                    {
+                        if (args.Length > 2)
+                        {
+                            foreach (string item in args[2].Split(';'))
+                            {
+                                programs[i][Convert.ToChar(item.Split('=')[0])] = item.Split('=')[1];
+                            }
+                        }
+                        Process startProg = new Process();
+                        startProg.StartInfo.FileName = '"' + programs[i]['p'] + '"';
+                        for (int j = 0; j < programs[i]['p'].Split('\\').Length - 1; j++)
+                        {
+                            startProg.StartInfo.WorkingDirectory += programs[i]['p'].Split('\\')[j] + "\\";
+                        }
+                        if (programs[i]['g'] != "")
+                        {
+                            startProg.StartInfo.Arguments = '"' + programs[i]['g'] + '"';
+                        }
+                        startProg.Start();
+                    }
+                    else
+                    {
+                        programs.RemoveAt(i);
+                        i--;
+                    }
+                }
+            }
+            while (true)
             {
                 Thread.Sleep(1000);
                 foreach (Process process in Process.GetProcesses())
@@ -56,6 +89,10 @@ namespace BoWind
                     {
                         if (process.ProcessName == value['n'])
                         {
+                            if (args.Length > 1)
+                            {
+                                Thread.Sleep(Convert.ToInt16(value['d']));
+                            }
                             string[] shift = value['s'].Split(',');
                             if (Convert.ToDouble(value['a'].Split(',')[0]) / Convert.ToDouble(value['a'].Split(',')[1]) >= monitorWidth / monitorHeight)
                             {
@@ -95,49 +132,24 @@ namespace BoWind
                                         GetWindowText(hand, stringBuilder, stringBuilder.Capacity);
                                         if (stringBuilder.ToString() != "" && stringBuilder.ToString() != "Default IME" && stringBuilder.ToString() != "MSCTFIME UI")
                                         {
-                                            ShowWindow(process.MainWindowHandle, 9);
-                                            SetWindowLong(hand, -16, 0x0008 | 0x00C);
-                                            SetWindowPos(hand, 0, Convert.ToInt16(monitorWidth / 2 - width / 2 - Convert.ToInt16(shift[0])), Convert.ToInt16(monitorHeight / 2 - height / 2 - Convert.ToInt16(shift[1])), Convert.ToInt16(width) + Convert.ToInt16(shift[2]), Convert.ToInt16(height) + Convert.ToInt16(shift[3]), 0x0400);
-                                            ShowWindow(hand, 5);
-                                            if (config["debug"] == "true")
-                                            {
-                                                Console.WriteLine(hand + " " + stringBuilder.ToString());
-                                            }
+                                            MoveWindow(hand, shift);
+                                            Console.WriteLine(hand + " " + stringBuilder.ToString());
                                         }
                                     }
                                 }
                             }
                             else
                             {
-                                ShowWindow(process.MainWindowHandle, 9);
-                                SetWindowLong(process.MainWindowHandle, -16, 0x0008 | 0x00C);
-                                SetWindowPos(process.MainWindowHandle, 0, Convert.ToInt16(monitorWidth / 2 - width / 2 - Convert.ToInt16(shift[0])), Convert.ToInt16(monitorHeight / 2 - height / 2 - Convert.ToInt16(shift[1])), Convert.ToInt16(width) + Convert.ToInt16(shift[2]), Convert.ToInt16(height) + Convert.ToInt16(shift[3]), 0x0400);
-                                ShowWindow(process.MainWindowHandle, 5);
-                                if (config["debug"] == "true")
-                                {
-                                    Console.WriteLine(process.MainWindowHandle + " " + process.MainWindowTitle);
-                                }
+                                MoveWindow(process.MainWindowHandle, shift);
+                                Console.WriteLine(process.MainWindowHandle + " " + process.MainWindowTitle);
                             }
-                            if (config["debug"] == "true")
-                            {
-                                Console.WriteLine(value['n'] + " compat " + value['c'] + " " + Convert.ToString(Convert.ToDouble(value['a'].Split(',')[0]) / Convert.ToDouble(value['a'].Split(',')[1])) + " " + Convert.ToInt32(monitorWidth / 2 - width / 2) + " - " + shift[0] + " " + Convert.ToInt16(monitorHeight / 2 - height / 2) + " - " + shift[1] + " " + width + " + " + shift[2] + " " + height + " + " + shift[3] + "\n");
-                            }
+                            Console.WriteLine(value['n'] + " compat " + value['c'] + " " + Convert.ToString(Convert.ToDouble(value['a'].Split(',')[0]) / Convert.ToDouble(value['a'].Split(',')[1])) + " " + Convert.ToInt32(monitorWidth / 2 - width / 2) + " - " + shift[0] + " " + Convert.ToInt16(monitorHeight / 2 - height / 2) + " - " + shift[1] + " " + width + " + " + shift[2] + " " + height + " + " + shift[3] + "\n");
                         }
                     }
                 }
                 if (config["debug"] == "true")
                 {
-                    command = Console.ReadLine().Split(' ');
-                    if (command[0] == "edit")
-                    {
-                        Process edit = new Process();
-                        edit.StartInfo.FileName = command[1] + ".txt";
-                        edit.Start();
-                    }
-                    if (command[0] == "reload")
-                    {
-                        loadFiles();
-                    }
+                    Console.ReadLine();
                 }
                 else
                 {
@@ -148,12 +160,37 @@ namespace BoWind
                 }
             }
         }
-        static void loadFiles()
+        static bool Check(string check, int type)
         {
-            int checkInt;
-            bool checkBool;
+            if (type == 0)
+            {
+                int checkInt;
+                if (!int.TryParse(check, out checkInt))
+                {
+                    return true;
+                }
+            }
+            else if (type == 1)
+            {
+                bool checkBool;
+                if (!bool.TryParse(check, out checkBool))
+                {
+                    return true;
+                }
+            }
+            else if (type == 2)
+            {
+                int checkInt;
+                if (!int.TryParse(check, NumberStyles.HexNumber, NumberFormatInfo.InvariantInfo, out checkInt))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        static void LoadConfig()
+        {
             config = new Dictionary<string, string>();
-            programs = new List<Dictionary<char, string>>();
             config.Add("debug", "false");
             config.Add("background", "false");
             config.Add("backgroundcolor", "000000");
@@ -165,55 +202,74 @@ namespace BoWind
                     config[temp[0]] = temp[1];
                 }
             }
-            if (!bool.TryParse(config["debug"], out checkBool))
+            if (Check(config["debug"], 1))
             {
                 config["debug"] = "false";
                 MessageBox.Show("Debug options ignored.");
             }
-            if (!bool.TryParse(config["background"], out checkBool))
+            if (Check(config["background"], 1))
             {
                 config["background"] = "False";
                 MessageBox.Show("Background options ignored.");
             }
-            if (!int.TryParse(config["backgroundcolor"], NumberStyles.HexNumber, NumberFormatInfo.InvariantInfo, out checkInt))
+            if (Check(config["backgroundcolor"], 2))
             {
                 config["backgroundcolor"] = "000000";
                 MessageBox.Show("Background color options ignored.");
+            }
+            if (Environment.GetCommandLineArgs().Length > 1)
+            {
+                config["debug"] = "false";
             }
             if (config["debug"] == "false")
             {
                 ShowWindow(Process.GetCurrentProcess().MainWindowHandle, 0);
             }
-            for (int i = 0; i < File.ReadAllLines("programs.txt").Length; i++)
+        }
+        static void LoadPrograms(string[] progConf)
+        {
+            for (int i = 0; i < progConf.Length; i++)
             {
-                programs.Add(new Dictionary<char, string> { { 'a', Convert.ToString(monitorWidth) + ',' + Convert.ToString(monitorHeight) }, { 'c', "false" }, { 'n', "default" }, { 's', "0,0,0,0" } });
-                string[] values = File.ReadAllLines("programs.txt")[i].Split('.');
+                programs.Add(new Dictionary<char, string> { { 'a', Convert.ToString(monitorWidth) + ',' + Convert.ToString(monitorHeight) }, { 'c', "false" }, { 'd', "0" }, { 'g', "" }, { 'n', "default" }, { 'p', "default" }, { 's', "0,0,0,0" } });
+                string[] values = progConf[i].Split(';');
                 for (int j = 0; j < values.Length; j++)
                 {
-                    if (values[j].Split(':').Length == 2)
+                    if (values[j].Split('=').Length == 2)
                     {
-                        if (programs[i].ContainsKey(Convert.ToChar(values[j].Split(':')[0])))
+                        if (programs[i].ContainsKey(Convert.ToChar(values[j].Split('=')[0])))
                         {
-                            programs[i][Convert.ToChar(values[j].Split(':')[0])] = values[j].Split(':')[1];
+                            programs[i][Convert.ToChar(values[j].Split('=')[0])] = values[j].Split('=')[1];
                         }
                     }
                 }
-                if (programs[i]['s'].Split(',').Length < 4 || !int.TryParse(programs[i]['s'].Split(',')[0], out checkInt) || !int.TryParse(programs[i]['s'].Split(',')[1], out checkInt) || !int.TryParse(programs[i]['s'].Split(',')[2], out checkInt) || !int.TryParse(programs[i]['s'].Split(',')[3], out checkInt))
-                {
-                    programs[i]['s'] = "0,0,0,0";
-                    MessageBox.Show("Border shift options for " + values[0] + " ignored.");
-                }
-                if (programs[i]['a'].Split(',').Length < 2 || !int.TryParse(programs[i]['a'].Split(',')[0], out checkInt) || !int.TryParse(programs[i]['a'].Split(',')[1], out checkInt))
+                if (programs[i]['a'].Split(',').Length < 2 || Check(programs[i]['a'].Split(',')[0], 0) || Check(programs[i]['a'].Split(',')[1], 0))
                 {
                     programs[i]['a'] = Convert.ToString(monitorWidth) + ',' + Convert.ToString(monitorHeight);
                     MessageBox.Show("Aspect ratio options for " + values[0] + " ignored.");
                 }
-                if (!bool.TryParse(programs[i]['c'], out checkBool))
+                if (Check(programs[i]['c'], 1))
                 {
                     programs[i]['c'] = "false";
                     MessageBox.Show("Compatibility options for " + values[0] + " ignored.");
                 }
+                if (Check(programs[i]['d'], 0))
+                {
+                    programs[i]['d'] = "0";
+                    MessageBox.Show("Compatibility options for " + values[0] + " ignored.");
+                }
+                if (programs[i]['s'].Split(',').Length < 4 || Check(programs[i]['s'].Split(',')[0], 0) || Check(programs[i]['s'].Split(',')[1], 0) || Check(programs[i]['s'].Split(',')[2], 0) || Check(programs[i]['s'].Split(',')[3], 0))
+                {
+                    programs[i]['s'] = "0,0,0,0";
+                    MessageBox.Show("Border shift options for " + values[0] + " ignored.");
+                }
             }
+        }
+        static void MoveWindow(IntPtr hand, string[] shift)
+        {
+            ShowWindow(hand, 9);
+            SetWindowLong(hand, -16, 0x0008 | 0x00C);
+            SetWindowPos(hand, 0, Convert.ToInt16(monitorWidth / 2 - width / 2 - Convert.ToInt16(shift[0])), Convert.ToInt16(monitorHeight / 2 - height / 2 - Convert.ToInt16(shift[1])), Convert.ToInt16(width) + Convert.ToInt16(shift[2]), Convert.ToInt16(height) + Convert.ToInt16(shift[3]), 0x0400);
+            ShowWindow(hand, 5);
         }
     }
 }
--- README.md
+++ README.md
@@ -1,45 +1,7 @@
-BoWind
-=========
+# <u>BoWind</u>
 Another borderless windowed fullscreen application.
 
-Downloads
-=========
+# <u>Downloads</u>
 The [release page](https://github.com/Wsheerio/BoWind/releases) should have a build of the most recent version.
 
-Run BoWind.exe to create config.txt and programs.txt or create the files yourself. With debug mode off simply run BoWind.exe after opening a game to have it resized.
-
-Documentation
-=========
-config.txt
-
-This is where you'll put your config settings, all entries are "option setting". Currently supports debug, background, and backgroundcolor which are bool, bool, and hex triplet respectively. Background and backgroundcolor are not implemented yet.
-
-    debug true
-    background false
-    backgroundcolor 0CD34E
-
-programs.txt
-
-This is where you'll put your program settings, all entries are separated with ".", options and settings are separated with ":". Currently supported options are a, c, n, and s which are aspect ratio (width,height), compatibility mode (bool), process name, and shift (x,y,w,h) respectively. Compatibility mode grabs all related processes and resizes those instead of just the mainwindowhandle, this helps with some games but you'll probably never need to use it. Shift moves the application x pixels left and y pixels up and increases the width by w and height by h. Defaults are you monitor's aspect ratio, compatibility off, "default" (if you get this wrong it obviously won't work), and 0,0,0,0.
-
-    n:DarkSoulsII.s:2,2,4,4
-    n:RustClient
-    n:DOMO
-    a:632,473.c:true.n:endless.s:0,25,0,25
-    
-Debug Menu
-
-If debug is set to true the console will not be hidden and will stay open until you close it. Pressing enter will loop through window resizing again. After each window resize it will list the process ids and titles of the windows resized then the configuration used to resize them. This line is read "processname compatibility true/false aspect ratio x y - shift width height + shift". Using the program config provided above after resizing DarkSoulsII.exe and endless.exe you would see.
-
-    (processid) DarkSoulsII
-    DarkSoulsII compat false 1.77777777777778 0 - 2 0 - 2 1920 + 4 1080 + 4
-    
-    (processid) EndlessOnline
-    (processid) Endless Online
-    endless compat true 1.33615221987315 238 - 0 0 - 25 1443.044397463 + 0 1080 + 25
-    
-There are two commands you can use if debug is set to true, they are edit and reload. Edit opens up the given text file in your default text editor, reload reloads all config files.
-
-    edit config
-    edit programs
-    reload
+Check out the [wiki](https://github.com/Wsheerio/BoWind/wiki) for information on how to use Bowind.
